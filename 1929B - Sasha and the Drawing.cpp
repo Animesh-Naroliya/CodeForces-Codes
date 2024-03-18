a88f05d35c8d1385da06fe313a80a144eabb322d
Animesh_Naroliya <111117478+Animesh-Naroliya@users.noreply.github.com>
2024-03-18 13:12:48 +0530

    1929B - Sasha and the Drawing.cpp

--- 1929B - Sasha and the Drawing.cpp
+++ 1929B - Sasha and the Drawing.cpp
@@ -3,6 +3,22 @@ using namespace std;
 
 int main()
 {
-    
+    long t;
+    cin >> t;
+    while (t--)
+    {
+        long n, k;
+        cin >> n >> k;
+        long ans = 2 * n;
+        if (k <= 4 * n - 4)
+        {
+            ans = (k + 1) / 2;
+        }
+        else if (k <= 4 * n - 3)
+        {
+            ans = 2 * n - 1;
+        }
+        cout << ans << endl;
+    }
     return 0;
 }
